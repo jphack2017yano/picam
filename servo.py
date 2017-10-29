3633ad7c63ed2e69fdae6ca6893ffca37f7411de
yuta_ozaki <y.ozakix@gmail.com>
2017-10-30 01:31:51 +0900

    [modify] servo.py変更
    0.1秒だけ移動します

--- servo.py
+++ servo.py
@@ -1,32 +1,7 @@
-import serial
+import serial, time
 
-class Servo(object):
-
-  def __init__(self, path_to_serial):
-    self.LR, self.UD = 90, 60
-    self.con = serial.Serial(path_to_serial,9600,timeout=10)
-
-  def turn(self):
-    # self.con.write( "U%03dU%03dL%03dL%03d" % (self.UD,self.UD,self.LR,self.LR) )
-    order = 'U' + str(self.UD) + 'U' + str(self.UD) + 'L' + str(self.LR) + 'L' + str(self.LR) 
-    self.con.write(order.encode('utf-8'))
-
-  def turn_left(self,resolution=10):
-    self.LR = self.LR-resolution if self.LR-resolution>0 else 0
-    self.turn()
-
-  def turn_right(self,resolution=10):
-    self.LR = self.LR+resolution if self.LR+resolution<180 else 180
-    self.turn()
-
-  def turn_up(self,resolution=10):
-    self.UD = self.UD-resolution if self.UD-resolution>0 else 0
-    self.turn()
-
-  def turn_down(self,resolution=10):
-    self.UD = self.UD+resolution if self.UD+resolution<180 else 180
-    self.turn()
-
-  def turn_front(self):
-    self.UD, self.LR = 90,60
-    self.turn();
+s = serial.Serial('/dev/ttyS0',9600,timeout=10)
+def move(order) :
+    s.write(order)
+    time.sleep(0.1)
+    s.write('s')
